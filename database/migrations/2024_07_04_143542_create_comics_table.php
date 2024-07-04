9fc359f3d56f359498f11be416bed9e65307fa2b
lucianocretella95 <luciano.cretella@tim.it>
2024-07-04 16:54:37 +0200

    comic table

--- database/migrations/2024_07_04_143542_create_comics_table.php
+++ database/migrations/2024_07_04_143542_create_comics_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('price');
+            $table->string('release_year');
             $table->timestamps();
         });
     }
